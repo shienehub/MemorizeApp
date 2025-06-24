f2af1fa849fa7c08842414a61efdeed0c94a7628
Shiene Park <whatwillmyversebe@naver.com>
2025-06-24 10:17:33 +0900

    Initial Commit

---
+++ MemorizeApp/MemorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppApp.swift
+//  MemorizeApp
+//
+//  Created by superUser on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
